a07ac75a0fe35e7b41c62eeddbe5fdcdfaa48c7c
emcclellan <85776223+emcclellan@users.noreply.github.com>
2023-03-15 22:10:16 -0700

    Updated voter to vote for 2 cards

--- pins-app/src/Voter/Voter.js
+++ pins-app/src/Voter/Voter.js
@@ -4,34 +4,81 @@ import './Voter.css';
 class Voter extends React.Component{
     constructor(props){
         super(props);
-        this.state = {score: 0};
+        this.state = {
+            score1: 0,
+            score2: 0,
+            winner: "undecided"
+        };
         
         this.increment = this.increment.bind(this);
         this.decrement = this.decrement.bind(this);
+        this.winner = this.getWinner.bind(this);
     }
 
     render(){
         return(
             <div>
-                <img src={require('../Images/hearthStone1.PNG')} alt="hearthStone1"/>
-                <div>{this.state.score}</div>
-                <button className="button-vote" onClick={this.increment}>Up</button>
-                <button className="button-vote" onClick={this.decrement}>Down</button>
+                <div>
+                    winner is: {this.state.winner}
+                </div>
+                <div className='row'>
+                    <div className='column'>
+                        <img src={require('../Images/hearthStone1.PNG')} alt="hearthStone1"/>
+                        <div>{this.state.score1}</div>
+                        <button className="button-vote" onClick={(e) => this.increment('1', e)}>Up</button>
+                        <button className="button-vote" onClick={(e) => this.decrement('1', e)}>Down</button>
+                    </div>
+                    <div className='column'>
+                        <img src={require('../Images/hearthStone2.PNG')} alt="hearthStone1"/>
+                        <div>{this.state.score2}</div>
+                        <button className="button-vote" onClick={(e) => this.increment('2', e)}>Up</button>
+                        <button className="button-vote" onClick={(e) => this.decrement('2', e)}>Down</button>
+                    </div>
+                </div>
             </div>
-
         );
     }
 
-    increment(){
-        this.setState(state => ({
-            score: state.score + 1 
-        }));
+    getWinner(){
+        if (this.state.score1 > this.state.score2){
+            this.setState(state => ({
+                winner: "Hearth Stone 1"
+            }));
+        }
+        else{
+            this.setState(state => ({
+                winner: "Hearth Stone 2"
+            }))
+        }
     }
 
-    decrement(){
-        this.setState(state => ({
-        score: state.score - 1
-        }));
+    increment(contestant){
+        if (contestant === '1'){
+            this.setState(state => ({
+                score1: state.score1 + 1
+            }));
+        }
+        else{
+            this.setState(state => ({
+                score2: state.score2 + 1
+            }));
+        }
+        //this.getWinner();
+    }
+
+    decrement(contestant){
+        
+        if (contestant === '1'){
+            this.setState(state => ({
+                score1: state.score1 - 1
+            }));
+        }
+        else{
+            this.setState(state => ({
+                score2: state.score2 - 1
+            }))
+        }
+        //this.getWinner();
     }
 }
 
